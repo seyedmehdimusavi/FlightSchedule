ef277d741626ff793b81bbaaae1a08e1a0a885fd
Mehdi Mousavi <m.mousavi@DadeKavan.Lan>
2023-06-15 18:39:39 +0330

    editStatus2

--- FlightSchedule/Services/FlightService.cs
+++ FlightSchedule/Services/FlightService.cs
@@ -5,10 +5,10 @@ using Microsoft.EntityFrameworkCore;
 using System.Text;
 using static FlightSchedule.model.FlightScheduleContext;
 
-    //public interface IFlightService
-    //{
-    //    public int calculateDepartureValues();
-    //}
+//public interface IFlightService
+//{
+//    public int calculateDepartureValues();
+//}
 //==========================================================================================================
 
 public class FlightService
@@ -19,11 +19,11 @@ public class FlightService
     {
         long? departureValue = db.flights.FirstOrDefault(t => t.flight_id == flight_id).departureValue;
         var ifNew = from f in db.flights
-                    where f.airline_id == airline_id && (f.departureValue > (departureValue - 10110)) && (f.departureValue <= departureValue)
+                    where f.airline_id == airline_id && (f.departureValue > (departureValue - 10110)) && (f.departureValue <= departureValue + 30)
                     select f;
 
         var ifDiscontinued = from f in db.flights
-                             where f.airline_id == airline_id && (f.departureValue > (departureValue + 10110)) && (f.departureValue >= departureValue)
+                             where f.airline_id == airline_id && (f.departureValue > (departureValue + 10110)) && (f.departureValue >= departureValue - 30)
                              select f;
 
         if (ifNew.Count() == 0) return "New flights";
@@ -63,27 +63,27 @@ public class FlightService
         int end_date = string2DataTime(end_date_str);
         int agency_id = int.Parse(agency_id_str);
         ResultModel[] res = (from f in db.flights
-                   join r in db.routes on f.route_id equals r.route_id
-                   join e in db.subscriptions on new { r.destination_city_id, r.origin_city_id } equals new { e.destination_city_id, e.origin_city_id }
-                   where e.agency_id == agency_id && f.departureValue >= start_date && f.departureValue <= end_date
-                   select new ResultModel
-                   {
-                       flight_id = f.flight_id,
-                       origin_city_id = e.origin_city_id,
-                       destination_city_id = e.destination_city_id,
-                       departure_time = f.departure_time,
-                       arrival_time = f.arrival_time,
-                       airline_id = f.airline_id,
-                       //status = "" //getStatus(f.flight_id, f.departure_time, f.airline_id)
-                   }).ToArray();
+                             join r in db.routes on f.route_id equals r.route_id
+                             join e in db.subscriptions on new { r.destination_city_id, r.origin_city_id } equals new { e.destination_city_id, e.origin_city_id }
+                             where e.agency_id == agency_id && f.departureValue >= start_date && f.departureValue <= end_date
+                             select new ResultModel
+                             {
+                                 flight_id = f.flight_id,
+                                 origin_city_id = e.origin_city_id,
+                                 destination_city_id = e.destination_city_id,
+                                 departure_time = f.departure_time,
+                                 arrival_time = f.arrival_time,
+                                 airline_id = f.airline_id,
+                                 //status = "" //getStatus(f.flight_id, f.departure_time, f.airline_id)
+                             }).ToArray();
 
-        Console.WriteLine("Number of Available Flights:{0}",res.Length);
-            
+        Console.WriteLine("Number of Available Flights:{0}", res.Length);
+
         StringBuilder result = new StringBuilder();
         result.Append("flight_id,origin_city_id,destination_city_id,departure_time,arrival_time,airline_id,status\n");
         foreach (var t in res)
         {
-            result.Append($"{t.flight_id},{t.origin_city_id},{t.destination_city_id},{t.departure_time},{t.arrival_time},{t.airline_id}, { getStatus(t.flight_id,t.departure_time,t.airline_id)}\n");
+            result.Append($"{t.flight_id},{t.origin_city_id},{t.destination_city_id},{t.departure_time},{t.arrival_time},{t.airline_id}, { getStatus(t.flight_id, t.departure_time, t.airline_id)}\n");
         }
         File.WriteAllText("results.csv", result.ToString());
     }
